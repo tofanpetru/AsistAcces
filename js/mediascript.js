8187ab42097b112a979815a8cce8880553d7aeaa
tofanpetru <tofanpetru12@gmail.com>
2021-06-27 00:01:18 +0300

    minor updates

--- js/mediascript.js
+++ js/mediascript.js
@@ -1,79 +1,25 @@
-window.onload = function () {
-    if ('speechSynthesis' in window)
-        with (speechSynthesis) {
+var voices = window.speechSynthesis.getVoices();
 
-            var playEle = document.querySelector('.play-asistent');
-            var pauseEle = document.querySelector('.pasue-asistent');
-            var stopEle = document.querySelector('.stop-asistent');
-            var flag = false;
+var sayit = function ()
+{
+    var msg = new SpeechSynthesisUtterance();
 
-            
-            if(document.querySelector('.play-asistent') != null && document.querySelector('.pasue-asistent') != null && document.querySelector('.stop-asistent') != null){
-                playEle.addEventListener('click', onClickPlay);
-                pauseEle.addEventListener('click', onClickPause);
-                stopEle.addEventListener('click', onClickStop);
-            }
+    msg.voice = voices[10]; // Note: some voices don't support altering params
+    msg.lang = 'ro-RO';
 
-            function onClickPlay() {
-                if (!flag) {
-                    flag = true;
-                    utterance = new SpeechSynthesisUtterance(document.querySelector('.play-content').innerHTML);
-
-                    utterance.lang = 'ro-RO';
-                    utterance.rate = 0.7;
-                    utterance.onend = function () {
-                        flag = false;
-                        playEle.className = pauseEle.className = '';
-                        stopEle.className = 'stopped';
-                    };
-                    playEle.className = 'played';
-                    stopEle.className = '';
-                    speak(utterance);
-                }
-                if (paused) {
-                    playEle.className = 'played';
-                    pauseEle.className = '';
-                    resume();
-                }
-            }
-
-            function onClickPause() {
-                if (speaking && !paused) {
-                    pauseEle.className = 'paused';
-                    playEle.className = '';
-                    pause();
-                }
-            }
-
-            function onClickStop() {
-                if (speaking) {
-                    stopEle.className = 'stopped';
-                    playEle.className = pauseEle.className = '';
-                    flag = false;
-                    cancel();
-
-                }
-            }
-
-        }
-
-    else { /* speech synthesis not supported */
-        msg = document.createElement('h5');
-        msg.textContent = "Detected no support for Speech Synthesis";
-        msg.style.textAlign = 'center';
-        msg.style.backgroundColor = 'red';
-        msg.style.color = 'white';
-        msg.style.marginTop = msg.style.marginBottom = 0;
-        document.body.insertBefore(msg, document.querySelector('div'));
-    }
-
+    return msg;
 }
 
-function getTextToSpeach(ctrl) {
-    var textInside = ctrl.querySelector("p").textContent;
-    textToSpeech(textInside);
-    console.log(textInside)
-  }
-  $(document).on("click", ".play-asistent", function(){
-      $(".play-asistent").textContent
-});
+
+var speekResponse = function (text)
+{
+    speechSynthesis.cancel(); // if it errors, this clears out the error.
+
+    var sentences = text.split(".");
+    for (var i=0;i< sentences.length;i++)
+    {
+        var toSay = sayit();
+        toSay.text = sentences[i];
+        speechSynthesis.speak(toSay);
+    }
+}
--- js/signasistent.js
+++ js/signasistent.js
@@ -3,16 +3,19 @@ $(document).on("click", ".close-asistent", function(){
 });
 
 function signAsistentAddHelper() {
-    var els = document.querySelectorAll("p, h5, h1, a, span");
+    var els = document.querySelectorAll("p, h5, h1, span, spec, h3");
     for(var i = 0, all = els.length; i < all; i++){   
          els[i].classList.add('signly-text-segment', 'play');
      }
 }
 function signAsistentRemoveHelper() {
-    var els = document.querySelectorAll("p, h5, h1, a, span");
-    for(var i = 0, all = els.length; i < all; i++){   
-         els[i].classList.remove('signly-text-segment', 'play');
-     }
+    if(!document.body.classList.contains("signly-text-segment")) {
+        var els = document.querySelectorAll("p, h5, h1, span, spec, h3");
+        for(var i = 0, all = els.length; i < all; i++){   
+            els[i].classList.remove('signly-text-segment', 'play');
+        }
+    }
+    
 }
 function signAsistentHelper()
 {
@@ -26,3 +29,33 @@ function signAsistentHelper()
         document.getElementById('web_asset').textContent = "web_asset";
     }
 }
+
+function getElementAttribute(elemID) {
+    var signPlayAttribute = 'sign-play';
+    if(elemID && document.getElementById(elemID).getAttribute(signPlayAttribute) && document.getElementById('web_asset').textContent.match("web_asset_off") != null){
+        var theElement = document.getElementById(elemID);
+    var theAttribute = theElement.getAttribute(signPlayAttribute);
+    console.log(theAttribute);
+    }
+}
+function getAllElementAttribute() {
+    document.querySelectorAll("[sign-play]").forEach(function(element) {
+        // element refers to the DOM node
+    
+        textToSpeech(element.getAttribute('sign-play'));
+    });
+}
+
+$(document).on("click", "p, h5, h1, span, h3, spec", function(){
+    var srcVideo = $(this).attr("sign-play") //cu index va fi in viitor
+    if(srcVideo && document.getElementById('web_asset').textContent.match("web_asset_off") != null){
+        $('.tap-target').tapTarget("open");
+        document.getElementById("asistent-p").src ="media/videos/" + srcVideo + ".mp4";
+    }
+
+    if(document.body.className.match("text_speech_on") != null && document.getElementById('web_asset').textContent.match("web_asset_off") != null){
+        var textContent = $(this).text();
+        speekResponse(textContent);
+        console.log(textContent);
+    }
+});
